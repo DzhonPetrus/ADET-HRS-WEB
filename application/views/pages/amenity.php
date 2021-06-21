3504d1a5e320af6eda5dcda80b6631ad4cee7657
Bryon Alindugan <bryonalindugan@gmail.com>
2021-06-21 19:49:04 +0800

    ame

---
+++ application/views/pages/amenity.php
@@ -0,0 +1,36 @@
+<!-- HEADER -->
+
+
+
+<!-- BODY -->
+
+
+<table id="myTable">
+  <thead>
+    <tr>
+      <th>
+        Amenities ID
+      </th>
+      <th>
+        Type
+      </th>
+      <th>
+        Description
+      </th>
+      <th>
+        Action
+      </th>
+    </tr>
+</thead>
+<tbody>
+</tbody>
+</table>
+
+
+
+<!-- Import JS-->
+<script src="<?= base_url('assets') ?>/js/pages/amenity.js"></script>
+
+
+
+
