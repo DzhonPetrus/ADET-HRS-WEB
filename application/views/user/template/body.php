cefce8534cd420fa0a42b253bef8906216d87aa3
John Peter Narzoles <dzhonpetrus@gmail.com>
2021-07-21 14:06:09 +0800

    Revert "Revert "Ar""

---
+++ application/views/user/template/body.php
@@ -0,0 +1,139 @@
+<main>
+
+<!-- slider Area Start-->
+<div class="slider-area ">
+    <!-- Mobile Menu -->
+    <div class="slider-active dot-style">
+        <div class="single-slider  hero-overly slider-height d-flex align-items-center" data-background="<?= base_url() ?>marian-master/assets/img/hero/landing1.jpg" >
+            <div class="container">
+                <div class="row justify-content-center text-center">
+                    <div class="col-xl-9">
+                        <div class="h1-slider-caption">
+                            <h1 data-animation="fadeInDown" data-delay=".4s">top hotel in the city</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-slider  hero-overly slider-height d-flex align-items-center" data-background="<?= base_url() ?>marian-master/assets/img/hero/h1_hero.jpg" >
+            <div class="container">
+                <div class="row justify-content-center text-center">
+                    <div class="col-xl-9">
+                        <div class="h1-slider-caption">
+                            <h1 data-animation="fadeInUp" data-delay=".4s">top hotel in the city</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-slider  hero-overly slider-height d-flex align-items-center" data-background="<?= base_url() ?>marian-master/assets/img/hero/landing3.jpg" >
+            <div class="container">
+                <div class="row justify-content-center text-center">
+                    <div class="col-xl-9">
+                        <div class="h1-slider-caption">
+                            <h1 data-animation="fadeInDown" data-delay=".4s">top hotel in the city</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- slider Area End-->
+
+<!-- Make customer Start-->
+<section class="make-customer-area customar-padding fix">
+    <div class="container-fluid p-0">
+        <div class="row">
+           <div class="col-xl-5 col-lg-6">
+                <div class="customer-img mb-120">
+                    <img src="<?= base_url() ?>marian-master/assets/img/customer/customar1.png" class="customar-img1" alt="">
+                    <img src="<?= base_url() ?>marian-master/assets/img/customer/customar2.png" class="customar-img2" alt="">
+                    <div class="service-experience heartbeat">
+                        <h3>25 Years of Service<br>Experience</h3>
+                    </div>
+                </div>
+           </div>
+            <div class=" col-xl-4 col-lg-4">
+                <div class="customer-caption">
+                    <span>About our company</span>
+                    <h2>Make the customer the hero of your story</h2>
+                    <div class="caption-details">
+                        <p class="pera-dtails">Lorem ipsum dolor sit amet, consectetur adipisic- ing elit, sed do eiusmod tempor inc. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. </p>
+                        <a href="about" class="btn more-btn1">Learn More <i class="ti-angle-right"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Make customer End-->
+
+<!-- Room Start -->
+<section class="room-area">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-8">
+                <!--font-back-tittle  -->
+                <div class="font-back-tittle mb-45">
+                    <div class="archivment-front">
+                        <h3>Our Rooms</h3>
+                    </div>
+                    <h3 class="archivment-back">Our Rooms</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <!-- Single Room -->
+                <div class="single-room mb-50">
+                    <div class="room-img">
+                       <a href="rooms.html"><img src="<?= base_url() ?>marian-master/assets/img/rooms/room2.jpg" alt=""></a>
+                    </div>
+                    <div class="room-caption">
+                        <h3><a href="rooms.html">Standard</a></h3>
+                        <div class="per-night">
+                            <span><u>₱</u>2000 <span>/ per night</span></span>
+                            <h4> 1 to 2 Persons</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <!-- Single Room -->
+                <div class="single-room mb-50">
+                    <div class="room-img">
+                       <a href="rooms.html"><img src="<?= base_url() ?>marian-master/assets/img/rooms/room4.jpg" alt=""></a>
+                    </div>
+                    <div class="room-caption">
+                        <h3><a href="rooms.html">Deluxe</a></h3>
+                        <div class="per-night">
+                            <span><u>₱</u>3000 <span>/ per night</span></span>
+                            <h4> 1 to 4 Persons</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-lg-6 col-md-6">
+                <!-- Single Room -->
+                <div class="single-room mb-50">
+                    <div class="room-img">
+                       <a href="rooms.html"> <img src="<?= base_url() ?>marian-master/assets/img/rooms/room6.jpg" alt=""></a>
+                    </div>
+                    <div class="room-caption">
+                        <h3><a href="rooms.html">Super Suite</a></h3>
+                        <div class="per-night">
+                            <span><u>₱</u>4000 <span>/ per night</span></span>
+                            <h4> 1 to 5 Persons</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- Room End -->
+
+</main>
