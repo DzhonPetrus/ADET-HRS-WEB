362bb876d13e84e1a03b8e6cf8e636b7c8c60cc0
arvincarino <carino.arvin0303@gmail.com>
2021-07-23 03:14:10 +0800

    profile and editprofile form

---
+++ application/views/user/pages/editprofile.php
@@ -0,0 +1,129 @@
+<div class="container">
+		<div class="main-body">
+            <br>
+			<div class="row">
+				<div class="col-lg-4">
+					<div class="card">
+						<div class="card-body">
+							<div class="d-flex flex-column align-items-center text-center">
+								<img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="110">
+								<div class="mt-3">
+									<h4>John Doe</h4>
+									<p class="text-primary mb-1">admin@admin.com</p>
+									<p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-8">
+					<div class="card">
+						<div class="card-body">
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Email:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="John Doe">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">First Name:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="john@example.com">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Middle Name:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(239) 816-9029">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Last Name:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+							<div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Birthdate:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="date" class="form-control" placeholder="Bay Area, San Francisco, CA">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Nationality:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Contact:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Street:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">City:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Postal Code:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">State:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+                            <div class="row mb-3">
+								<div class="col-sm-3">
+									<h6 class="form-control-label">Country:</h6>
+								</div>
+								<div class="col-sm-9 text-secondary">
+									<input type="text" class="form-control" placeholder="(320) 380-4539">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-3"></div>
+								<div class="col-sm-9 text-secondary">
+									<input type="button" class="btn btn-primary px-4" value="Save Changes">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
---
+++ application/views/user/pages/profile.php
@@ -0,0 +1,139 @@
+<div class="container">
+    <div class="main-body">
+          <br>
+          <div class="row gutters-sm">
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-center text-center">
+                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                    <div class="mt-3">
+                      <h4>John Doe</h4>
+                      <p class="text-primary mb-1">admin@admin.com</p>
+                      <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-8">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Email:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      ADMIN@ADMIN.COM
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">First Name:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      JOHN
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Middle Name:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      CASTRO
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Last Name:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      DOE
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Birthdate:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      03/04/1968
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Nationality:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      Filipino
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Contact Number:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      09123456789
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Street:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      15 J.P RIZAL ST.
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">City:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      Quezon City
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Postal Code:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      1123
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">State:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      Bay Area, San Francisco, CA
+                    </div>
+                  </div><hr>
+                  <div class="row">
+                    <div class="col-sm-2">
+                      <h6 class="form-control-label">Country:</h6>
+                    </div>
+                    <div class="col-sm-9 text-primary">
+                      Philippines
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-12">
+                      <a class="btn btn-primary " href="<?= base_url() ?>user/editprofile">Edit</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
