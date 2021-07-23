b89403883f2c0a382201878a374f6ac02bf4c6dc
arvincarino <carino.arvin0303@gmail.com>
2021-07-23 16:33:49 +0800

    new profile and edit profile UI

--- application/views/user/pages/profile.php
+++ application/views/user/pages/profile.php
@@ -1,139 +1,196 @@
+ <!-- Header -->
+ <div class="header pb-6 d-flex align-items-center" style="min-height: 500px; background-image: url(../assets/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
+        <!-- Mask -->
+        <span class="mask bg-gradient-default opacity-8"></span>
+        <!-- Header container -->
+        <div class="container-fluid d-flex align-items-center">
+          <div class="row">
+            <div class="col-lg-7 col-md-10">
+              <h1 class="display-2 text-white">Hello Jesse</h1>
+              <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
+              <a href="#!" class="btn btn-neutral">Edit profile</a>
+            </div>
+          </div>
+        </div>
+      </div>
 <div class="container">
     <div class="main-body">
           <br>
-          <div class="row gutters-sm">
-            <div class="col-md-4 mb-3">
-              <div class="card">
-                <div class="card-body">
-                  <div class="d-flex flex-column align-items-center text-center">
-                    <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
-                    <div class="mt-3">
-                      <h4>John Doe</h4>
-                      <p class="text-primary mb-1">admin@admin.com</p>
-                      <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                    </div>
-                  </div>
+          <div class="container-fluid mt--6"></div>
+      <div class="row">
+        <div class="col-xl-4 order-xl-2">
+          <div class="card card-profile">
+            <div class="row justify-content-center">
+              <div class="col-lg-3 order-lg-2">
+                <div class="card-profile-image">
+                  <a href="#">
+                    <img src="../assets/img/theme/team-4.jpg" class="rounded-circle">
+                  </a>
                 </div>
               </div>
             </div>
-            <div class="col-md-8">
-              <div class="card mb-3">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Email:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      ADMIN@ADMIN.COM
-                    </div>
+            <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
+              <div class="d-flex justify-content-between">
+              </div>
+            </div>
+            <div class="card-body pt-0">
+              <div class="row">
+                <div class="col">
+                  <div class="card-profile-stats d-flex justify-content-center">
                   </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">First Name:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      JOHN
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Middle Name:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      CASTRO
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Last Name:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      DOE
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Birthdate:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      03/04/1968
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Nationality:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      Filipino
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Contact Number:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      09123456789
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Street:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      15 J.P RIZAL ST.
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">City:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      Quezon City
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Postal Code:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      1123
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">State:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      Bay Area, San Francisco, CA
-                    </div>
-                  </div><hr>
-                  <div class="row">
-                    <div class="col-sm-2">
-                      <h6 class="form-control-label">Country:</h6>
-                    </div>
-                    <div class="col-sm-9 text-primary">
-                      Philippines
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <a class="btn btn-primary " href="<?= base_url() ?>user/editprofile">Edit</a>
-                    </div>
-                  </div>
+                </div>
+              </div>
+              <div class="text-center">
+                <h5 class="h3">
+                  Jessica Jones
+                </h5>
+                <div class="h5 mt-4">
+                  <i class="ni business_briefcase-24 mr-2"></i>admin@admin.com
+                </div>
+                <div>
+                  <i class="ni education_hat mr-2"></i>Bucharest, Romania
                 </div>
               </div>
             </div>
           </div>
         </div>
+        <div class="col-xl-8 order-xl-2">
+          <div class="card">
+            <div class="card-header">
+              <div class="row align-items-center">
+                <div class="col-8">
+                  <h3 class="mb-0"> Profile </h3>
+                </div>
+              </div>
+            </div>
+            <div class="card-body">
+              <form>
+                <h6 class="heading-small text-muted mb-4">User information</h6>
+                <div class="pl-lg-4">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-user-email">Email</label>
+                  <h4 class="mb-0">admin@admin.com</h4>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-user-first-name">First Name</label>
+                  <h4 class="mb-0">Admin John</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-user-middle-name">Middle Name</label>
+                  <h4 class="mb-0">Concepcion</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-user-last-name">Last Name</label>
+                  <h4 class="mb-0">Carino</h4>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-user-birthdate">Birthdate</label>
+                  <h4 class="mb-0">03/03/1999</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-user-nationality">Nationality</label>
+                  <h4 class="mb-0">Filipino</h4>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-contact-number">Contact Number</label>
+                  <h4 class="mb-0">091234567</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+                <!-- Address -->
+                <hr class="my-4">
+          <h6 class="heading-small text-muted mb-4">Address</h6>
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-Street">Street</label>
+                  <h4 class="mb-0">J.P Rizal ST.</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-city">City</label>
+                  <h4 class="mb-0">Quezon City</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-postal-code">Postal code</label>
+                  <h4 class="mb-0">1211</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-state">State</label>
+                  <h4 class="mb-0">admin@admin.com</h4>
+                </div>
+              </div>
+                <div class="col-lg-4">
+                  <div class="form-group">
+                    <label class="form-control-label" for="input-country">Country</label>
+                    <h4 class="mb-0">Philippines</h4>
+                  </div>
+                </div>
+            </div>
+             <!-- Address2 -->
+            <hr class="my-4">
+          <h6 class="heading-small text-muted mb-4">Address 2</h6>
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-Street">Street</label>
+                  <h4 class="mb-0">J.P Rizal ST.</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-city">City</label>
+                  <h4 class="mb-0">Quezon City</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-postal-code">Postal code</label>
+                  <h4 class="mb-0">1211</h4>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="form-group">
+                  <label class="form-control-label" for="input-state">State</label>
+                  <h4 class="mb-0">admin@admin.com</h4>
+                </div>
+              </div>
+                <div class="col-lg-4">
+                  <div class="form-group">
+                    <label class="form-control-label" for="input-country">Country</label>
+                    <h4 class="mb-0">Philippines</h4>
+                  </div>
+                </div>
+            </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
+</div>
